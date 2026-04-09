4a7e492304317904e331fbcc932c1b3a55841244
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-09 18:43:28 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,6 @@ import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
 import ImageCarousel from "@/components/ImageCarousel";
 import ResultsSection from "@/components/ResultsSection";
-import CTASection from "@/components/CTASection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
@@ -17,7 +16,6 @@ const Index = () => {
       <ServicesSection />
       <ImageCarousel />
       <ResultsSection />
-      <CTASection /> {/* 🔥 AQUI */}
       <ContactSection />
       <Footer />
     </div>
