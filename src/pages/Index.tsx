b4e5b4c720857d6dbbe53c1884c34b7a9efcc5b5
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-09 19:11:05 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,18 +6,39 @@ import ImageCarousel from "@/components/ImageCarousel";
 import ResultsSection from "@/components/ResultsSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 const Index = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background">
+
+      {/* Header */}
       <Navbar />
+
+      {/* Hero */}
       <HeroSection />
+
+      {/* Sobre (credibilidade) */}
       <AboutSection />
+
+      {/* Como funciona (método) */}
       <ServicesSection />
+
+      {/* Visual / autoridade */}
       <ImageCarousel />
+
+      {/* Resultados */}
       <ResultsSection />
+
+      {/* Contato (conversão) */}
       <ContactSection />
+
+      {/* Footer */}
       <Footer />
+
+      {/* Botão WhatsApp (fixo) */}
+      <WhatsAppButton />
+
     </div>
   );
 };
