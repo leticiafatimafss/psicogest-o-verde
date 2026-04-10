a03b455c5d2f8118bc5e97ffebf52a62232e064c
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 19:03:05 -0300

    Refactor Index component to include NR01Section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,8 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import ImageCarousel from "@/components/ImageCarousel";
 import AboutSection from "@/components/AboutSection";
-import NR01Section from "@/components/NR01Section"; // 👈 ADD AQUI
 import ServicesSection from "@/components/ServicesSection";
-import ResultsSection from "@/components/ResultsSection";
+import NR01Section from "@/components/NR01Section";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
@@ -12,37 +10,14 @@ import WhatsAppButton from "@/components/WhatsAppButton";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
-
-      {/* Header */}
       <Navbar />
-
-      {/* Hero */}
       <HeroSection />
-
-      {/* Fotos / autoridade visual */}
-      <ImageCarousel />
-
-      {/* Sobre (credibilidade) */}
       <AboutSection />
-
-      {/* 🚨 NR-01 (dor + urgência + valor) */}
-      <NR01Section />
-
-      {/* Como funciona (método) */}
       <ServicesSection />
-
-      {/* Resultados */}
-      <ResultsSection />
-
-      {/* Contato (conversão) */}
+      <NR01Section />
       <ContactSection />
-
-      {/* Footer */}
       <Footer />
-
-      {/* Botão WhatsApp (fixo) */}
       <WhatsAppButton />
-
     </div>
   );
 };
