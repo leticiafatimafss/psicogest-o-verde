e27aa3e2448b46efbafc4d3742634d90da5abc2b
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 18:00:48 -0300

    Add ImageCarousel component to Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import ImageCarousel from "@/components/ImageCarousel";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
 import ResultsSection from "@/components/ResultsSection";
@@ -12,6 +13,7 @@ const Index = () => {
     <div className="min-h-screen bg-background">
       <Navbar />
       <HeroSection />
+      <ImageCarousel />
       <AboutSection />
       <ServicesSection />
       <ResultsSection />
