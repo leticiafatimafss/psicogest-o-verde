ec35e90dfa93fde8c487ed2a601a57b799e25d6e
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 18:02:33 -0300

    Reintroduce ImageCarousel component in Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import ImageCarousel from "@/components/ImageCarousel";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
+import ImageCarousel from "@/components/ImageCarousel";
 import ResultsSection from "@/components/ResultsSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
@@ -11,15 +11,34 @@ import WhatsAppButton from "@/components/WhatsAppButton";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
+
+      {/* Header */}
       <Navbar />
+
+      {/* Hero */}
       <HeroSection />
-      <ImageCarousel />
+
+      {/* Sobre (credibilidade) */}
       <AboutSection />
+
+      {/* Como funciona (método) */}
       <ServicesSection />
+
+      {/* Visual / autoridade */}
+      <ImageCarousel />
+
+      {/* Resultados */}
       <ResultsSection />
+
+      {/* Contato (conversão) */}
       <ContactSection />
+
+      {/* Footer */}
       <Footer />
+
+      {/* Botão WhatsApp (fixo) */}
       <WhatsAppButton />
+
     </div>
   );
 };
