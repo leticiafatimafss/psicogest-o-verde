30d8e40ee5c6321ed2c3401140fba5be7da1aaaa
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 19:40:35 -0300

    Replace NR01Section with ImageCarousel in Index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
-import NR01Section from "@/components/NR01Section";
+import ImageCarousel from "@/components/ImageCarousel";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
@@ -14,7 +14,7 @@ const Index = () => {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
-      <NR01Section />
+      <ImageCarousel />
       <ContactSection />
       <Footer />
       <WhatsAppButton />
