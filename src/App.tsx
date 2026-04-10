38dc1ed0fcce3caf32ea3ef2acab75977ad1af95
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 16:36:09 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-
-import HomePage from "./pages/HomePage";
-import SobrePage from "./pages/SobrePage";
-import ServicosPage from "./pages/ServicosPage";
-import NR01Page from "./pages/NR01Page";
-import ContatoPage from "./pages/ContatoPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,11 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/sobre" element={<SobrePage />} />
-          <Route path="/servicos" element={<ServicosPage />} />
-          <Route path="/nr01" element={<NR01Page />} />
-          <Route path="/contato" element={<ContatoPage />} />
+          <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
