e00456822faffcf730ae9f43ade86349b361fcac
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 19:45:44 -0300

    Add NR01Section component to Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
+import NR01Section from "@/components/NR01Section";
 import ImageCarousel from "@/components/ImageCarousel";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
@@ -14,6 +15,7 @@ const Index = () => {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
+      <NR01Section />
       <ImageCarousel />
       <ContactSection />
       <Footer />
