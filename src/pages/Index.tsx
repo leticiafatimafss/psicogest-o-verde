a2731c5b5891046c3df52831f5875978abb2799d
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 18:15:08 -0300

    Remove duplicate ImageCarousel component
    Removed duplicate ImageCarousel import and usage.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import ImageCarousel from "@/components/ImageCarousel";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
-import ImageCarousel from "@/components/ImageCarousel";
 import ResultsSection from "@/components/ResultsSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
@@ -18,14 +18,14 @@ const Index = () => {
       {/* Hero */}
       <HeroSection />
 
+      {/* Fotos / autoridade visual */}
+      <ImageCarousel />
+
       {/* Sobre (credibilidade) */}
       <AboutSection />
 
       {/* Como funciona (método) */}
       <ServicesSection />
-
-      {/* Visual / autoridade */}
-      <ImageCarousel />
 
       {/* Resultados */}
       <ResultsSection />
