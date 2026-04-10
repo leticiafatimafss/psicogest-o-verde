e4f1fdf42a58a2fe0194a39e8fee3c009d53bf2d
leticiafatimafss <leticiafatimafss@gmail.com>
2026-04-10 11:29:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+
+import HomePage from "./pages/HomePage";
+import SobrePage from "./pages/SobrePage";
+import ServicosPage from "./pages/ServicosPage";
+import NR01Page from "./pages/NR01Page";
+import ContatoPage from "./pages/ContatoPage";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -15,8 +20,11 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sobre" element={<SobrePage />} />
+          <Route path="/servicos" element={<ServicosPage />} />
+          <Route path="/nr01" element={<NR01Page />} />
+          <Route path="/contato" element={<ContatoPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
