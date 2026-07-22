07acf12f908c069a76b0e19b7bd09d214d0a4374
inngridandrade <ingridgabriela.andrade@gmail.com>
2026-07-22 18:14:30 -0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-import AboutHero from "@/components/about/AboutHero";
-import AboutHistory from "@/components/about/AboutHistory";
-import MissionVisionValues from "@/components/about/MissionVisionValues";
-import DifferentialsSection from "@/components/about/DifferentialsSection";
-import TeamSection from "@/components/about/TeamSection";
+import AboutHero from "@/components/AboutHero";
+import AboutHistory from "@/components/AboutHistory";
+import MissionVisionValues from "@/components/MissionVisionValues";
+import DifferentialsSection from "@/components/DifferentialsSection";
+import TeamSection from "@/components/TeamSection";
 
 const AboutSection = () => {
   return (
