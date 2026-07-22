f98604b5b235949be0bbfdd8b3fd290062940a93
inngridandrade <ingridgabriela.andrade@gmail.com>
2026-07-22 17:18:43 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import AboutSection from "./pages/AboutSection";
+import Solutions from "./pages/Solutions";
+import Methodology from "./pages/Methodology";
+import Services from "./pages/Services";
+import Materials from "./pages/Materials";
+import Ebooks from "./pages/Ebooks";
+import Platform from "./pages/Platform";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -17,8 +21,32 @@ const App = () => (
       <BrowserRouter>
         <Routes>
   <Route path="/" element={<Index />} />
-  <Route path="/sobre" element={<AboutSection />} />
-  <Route path="*" element={<NotFound />} />
+
+<Route path="/sobre" element={<AboutSection />} />
+
+<Route path="/solucoes" element={<Solutions />} />
+<Route
+  path="/solucoes/metodologia"
+  element={<Methodology />}
+/>
+<Route
+  path="/solucoes/servicos"
+  element={<Services />}
+/>
+
+<Route path="/materiais" element={<Materials />} />
+<Route
+  path="/materiais/ebooks"
+  element={<Ebooks />}
+/>
+<Route
+  path="/materiais/plataforma"
+  element={<Platform />}
+/>
+
+<Route path="/contato" element={<Contact />} />
+
+<Route path="*" element={<NotFound />} />
 </Routes>
       </BrowserRouter>
     </TooltipProvider>
