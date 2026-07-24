6b944d7885068f6f831ba97cb12bc5866c24b5fa
leticiafatimafss <leticiafatimafss@gmail.com>
2026-07-24 16:57:07 -0300

    Create EbookNR1.tsx

---
+++ src/pages/EbookNR1.tsx
@@ -0,0 +1,9 @@
+import EbookHero from "@/components/ebook/EbookHero";
+
+export default function EbookNR1() {
+  return (
+    <main className="min-h-screen bg-background">
+      <EbookHero />
+    </main>
+  );
+}
