d2e9cb7008588ff007ad2fcd857bba0e19a8c925
leticiafatimafss <leticiafatimafss@gmail.com>
2026-07-24 18:57:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Index from "./pages/Index";
+import EbookNR1 from "./pages/EbookNR1";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -13,9 +15,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+
+          <Route path="/guia-nr1" element={<EbookNR1 />} />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
