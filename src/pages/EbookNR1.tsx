12918c66c928346782f13b9f3b7b02525d8efcc3
leticiafatimafss <leticiafatimafss@gmail.com>
2026-07-24 18:49:42 -0300

    Remove background class from main element

--- src/pages/EbookNR1.tsx
+++ src/pages/EbookNR1.tsx
@@ -2,7 +2,7 @@ import EbookHero from "@/components/ebook/EbookHero";
 
 export default function EbookNR1() {
   return (
-    <main className="min-h-screen bg-background">
+    <main className="min-h-screen">
       <EbookHero />
     </main>
   );
