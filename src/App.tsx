b5f39295386ad08acba18562f581902af9fd0a1f
leticiafatimafss <leticiafatimafss@gmail.com>
2026-07-29 14:13:54 -0300

    Add Metodologia route and import to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Index from "./pages/Index";
 import EbookNR1 from "./pages/EbookNR1";
+import Index from "./pages/Index";
+import Metodologia from "./pages/Metodologia";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,6 +20,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+
+          <Route path="/metodologia" element={<Metodologia />} />
 
           <Route path="/guia-nr1" element={<EbookNR1 />} />
 
